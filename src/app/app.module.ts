bb7b2361c2eaee38dfc477edb6fdcee8a27b41b9
vaibhav6776 <vaibhavbajpayee2109@gmail.com>
2020-12-06 23:22:09 +0530

    creattion of table using angular material, integration of the BE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CityTableComponent } from './city-table/city-table.component';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSortModule} from '@angular/material/sort';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CityTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/city-table/city-table.component.ts
@@ -0,0 +1,90 @@
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {environment} from '../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {timeout} from 'rxjs/operators';
+
+export interface UserData {
+  id: number;
+  city: string;
+  start_date: string;
+  end_date: string;
+  price: string;
+  status: string;
+  color: string;
+}
+
+// const COLORS = ['maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple',
+//   'fuchsia', 'lime', 'teal', 'aqua', 'blue', 'navy', 'black', 'gray'];
+// const NAMES = ['Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack',
+//   'Charlotte', 'Theodore', 'Isla', 'Oliver', 'Isabella', 'Jasper',
+//   'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'];
+
+@Component({
+  selector: 'app-city-table',
+  templateUrl: './city-table.component.html',
+  styleUrls: ['./city-table.component.css']
+})
+export class CityTableComponent implements OnInit, AfterViewInit {
+  displayedColumns = ['city', 'startdate', 'enddate' , 'price', 'status' , 'delete'];
+  dataSource: MatTableDataSource<UserData>;
+  users: UserData[] = [];
+  serverUrl: string;
+  isLoading = false;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  // tslint:disable-next-line:typedef
+  ngAfterViewInit() {
+    // this.dataSource.paginator = this.paginator;
+    // this.dataSource.sort = this.sort;
+  }
+
+  constructor(private http: HttpClient) {
+    this.serverUrl = environment.serverUrl;
+    // Assign the data to the data source for the table to render
+    this.getAllCities();
+  }
+
+  ngOnInit(): void {
+  }
+
+  getAllCities = () => {
+    this.isLoading = true;
+    this.http
+      .get(this.serverUrl + 'cities', {})
+      .subscribe((data) => {
+        console.log(data);
+        // @ts-ignore
+        this.dataSource = new MatTableDataSource(data);
+        setTimeout(() => {
+          console.log('hide');
+          this.dataSource.paginator = this.paginator;
+          this.dataSource.sort = this.sort;
+          this.isLoading = false;
+        }, 1000);
+      });
+  }
+
+  updateCity = () => {
+  }
+
+  deleteRecord = id => {
+    console.log('city id', id);
+    this.http
+      .delete(this.serverUrl + 'city/' + id, {})
+      .subscribe((data) => {
+        console.log(data);
+        this.getAllCities();
+      });
+  }
+
+
+  applyFilter = (filterValue: string) => {
+
+  }
+
+}
+
